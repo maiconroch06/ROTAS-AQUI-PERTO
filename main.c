a73093b11d7ce406e188604bf3b4e8b428b1b00d
Maicon_Rocha <maiconpablo588@gmail.com>
2025-08-18 12:46:20 -0300

    Deletando versão anterior do projeto
    Foi mantido apenas o arquivo main.c, em relação aos outros arquivos não conseguimos executar. Decidimos de ultima hora pedir ajuda ao GPT, e no fim ele "resolveu". Nosso professor comentou que possivelmente foi por conta de não termos compilado todos os arquivos de uma vez: "gcc main.c exibir.c (...) -o main.c".

--- main.c
+++ main.c
@@ -1,73 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "cadastro.h"
-#include "matriz.h"
-#include "exibir.h"
+#include "prototipos.h"
+
+matrizAdj matrizADJ;
 
 int main(void) {
     int escolha;
-    matrizAdj matrizADJ;
+
+    do{
+        escolha = CadastroCidades(&matrizADJ);
+    } while (escolha != 1);
+
+    int N = matrizADJ.n;
+    double d[N][N];
+    int p[N][N];
+
+    floydWarshall(&matrizADJ, N, d, p);
 
     do {
         system("cls");
         printf("\n ============= ROTAS-AQUI-PERTO =============");
-        printf("\n > [1] Adicionar Cidades");
+        printf("\n > [1] Alterar Cadastro de Cidades");
         printf("\n > [2] Exibir Matrizes");
-        printf("\n > [3] Calcular Rota");
+        printf("\n > [3] Salvar Dados");
+        printf("\n > [4] Calcular Rota");
         printf("\n > [0] Sair do Programa");
         printf("\n ============================================\n");
         printf(" Opcao: ");
-        scanf("%i",&escolha);
-        switch (escolha){
-        
-        case 1:
-            // (chamar aquivo cadastro)
-            CadastroCidades(&matrizADJ);
-            
-            /*for(int i = 0; i < matrizADJ.n; i++){
-                for (int j = 0; j < matrizADJ.n; j++) {
-                    printf("|%.2lf|", matrizADJ.matriz[i][j]);
-                }
-                printf("\n");
-            }*/
-            break;
-            
-        //*/
-        case 2:
-            // (chamar aquivo exibir)
-            ExibirMatrizes(&matrizADJ);
+        scanf("%d", &escolha);
 
-            break;
+        switch (escolha) {
+            case 1:
+                CadastroCidades(&matrizADJ);
+                N = matrizADJ.n;
+                floydWarshall(&matrizADJ, N, d, p);
+                break;
 
-        case 3: 
-            do {
-                printf("\n =========== CALCULAR-ROTA ===========");
-                printf("\n > Seu Ponto de Partida: ");
-                scanf("%i", &escolha);
+            case 2:
+                ExibirMatrizes(&matrizADJ, N, d, p);
+                break;
 
-                printf("\n > Seu Destino: ");
-                scanf("%i", &escolha);
+            case 3:
+                Salvar(&matrizADJ, N, d, p);
+                break;
 
-                printf("\n > [0] Cancelar");
-                printf("\n ================================");
-                printf("\n Opcao: ");
-                scanf("%i", &escolha);
-                
-                } while (1);
-                
-                break;//*/
-                
-        case 0:
-            // saindo
-            return 0;
-            
-        default:
-            printf(" ERRO: escolha fora do escopo");
-            break;
+            case 4:
+                CalcularRota(&matrizADJ, N, d, p);
+                break;
+
+            case 0:
+                return 0;
+
+            default:
+                break;
         }
-        
-        system("cls");
-
     } while (1);
-
 }
