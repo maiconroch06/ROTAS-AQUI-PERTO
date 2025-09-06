962ad564d65d17927802d136b86152c8606815c7
Maicon_Rocha <maiconpablo588@gmail.com>
2025-09-06 00:13:03 -0300

    mudanças no main
    no main, se localiza as funções principais do projeto. foi removido e alterado alguns parametros e adicionados duas novas funções, PrimeiroAcesso(): restringe ao usuario cadastrar as cidades antes de permitir as outras funcionalidades para o usuario. MenuPrincipal(): o nome diz por si só ;)

--- main.c
+++ main.c
@@ -2,57 +2,22 @@
 #include <stdlib.h>
 #include "prototipos.h"
 
-matrizAdj matrizADJ;
-
 int main(void) {
-    int escolha;
+    DadosCidades cidades;
 
-    do{
-        escolha = CadastroCidades(&matrizADJ);
-    } while (escolha != 1);
-
-    int N = matrizADJ.n;
-    double d[N][N];
-    int p[N][N];
-
-    floydWarshall(&matrizADJ, N, d, p);
+    PrimeiroAcesso(&cidades);
 
     do {
-        system("cls");
-        printf("\n ============= ROTAS-AQUI-PERTO =============");
-        printf("\n > [1] Alterar Cadastro de Cidades");
-        printf("\n > [2] Exibir Matrizes");
-        printf("\n > [3] Salvar Dados");
-        printf("\n > [4] Calcular Rota");
-        printf("\n > [0] Sair do Programa");
-        printf("\n ============================================\n");
-        printf(" Opcao: ");
-        scanf("%d", &escolha);
+        int opcao_menu = MenuPrincipal();
 
-        switch (escolha) {
-            case 1:
-                CadastroCidades(&matrizADJ);
-                N = matrizADJ.n;
-                floydWarshall(&matrizADJ, N, d, p);
-                break;
-
-            case 2:
-                ExibirMatrizes(&matrizADJ, N, d, p);
-                break;
-
-            case 3:
-                Salvar(&matrizADJ, N, d, p);
-                break;
-
-            case 4:
-                CalcularRota(&matrizADJ, N, d, p);
-                break;
-
-            case 0:
-                return 0;
-
-            default:
-                break;
+        switch (opcao_menu) {
+            case 1: CadastroCidades(&cidades); break;
+            case 2: ExibirMatrizes(&cidades); break;
+            case 3: Salvar(&cidades); break;
+            case 4: CalcularRota(&cidades); break;
+            case 0: limparTela(); return 0;
+            default: break;
         }
+
     } while (1);
 }
